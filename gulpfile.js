8059c990a18f3d535864ae90a154cf2396e91099
sadiejay <19538219+sadiejay@users.noreply.github.com>
2018-04-16 22:42:36 -0400

    Updates the gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,35 @@
-// npm install gulp-concat gulp-uglify gulp-rename browser-sync --save-dev
+// npm install gulp-sass browser-sync --save-dev
 
 
 // Include gulp
 var gulp = require('gulp');
 
 // Include Our Plugins
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
+var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
 
-
-// Concatenate JS
-//gulp.task('scripts', function() {
-//    return gulp.src('js/*.js')
-//        .pipe(concat('all.js'))
-//        .pipe(gulp.dest('js/'));
-//});
-
-
 ////////
-// localhost address with a port + watching scss/html files
-gulp.task('serve', function() {
+// directory server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
 
     browserSync.init({
         server:true
     });
-
     gulp.watch("**/*.css").on('change', browserSync.reload);
-    gulp.watch('js/*.js');
-    gulp.watch("**/*.html").on('change', browserSync.reload);
+    gulp.watch("scss/*.scss", ['sass']);
+    gulp.watch("app/*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("css/dist.css"))
+        .pipe(browserSync.stream());
 });
 /////////
+
 
 // Default Task
 gulp.task('default', ['serve']);
